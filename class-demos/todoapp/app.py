caa263eae170cbe66c2c7ae8a4b936a7a92eee6e
Guru Kuppuswamy <guruprasad.npk@gmail.com>
2021-01-09 22:55:50 -0800

    1. Add checkboxes for todos 2. Add update functionality

--- class-demos/todoapp/app.py
+++ class-demos/todoapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, abort
+from flask import Flask, render_template, request, jsonify, abort, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sys
@@ -7,42 +7,59 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'postgresql://postgres:test123@localhost:5432/todoapp'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
-class Todo(db.Model):
-  __tablename__ = 'todos'
-  id = db.Column(db.Integer, primary_key=True)
-  description = db.Column(db.String(), nullable=False)
-  completed = db.Column(db.String(), nullable=False, default=False)
 
-  def __repr__(self):
-      return f'<Todo ID: {self.id}, description: {self.description}>'
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean(), nullable=False, default=False)
+
+    def __repr__(self):
+        return f'<Todo ID: {self.id}, description: {self.description}>'
+
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  error = False
-  try:
-    description = request.get_json()['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    body = {
-      'description': todo.description
-    }
-  except:
-    error = True
-    db.session.rollback()
-    print(sys.exc_info())
-  finally:
-    db.session.close()
-  if error:
-    abort(400)
-  else:
-    return jsonify(body)
+    error = False
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body = {
+            'description': todo.description
+        }
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 
 @app.route('/')
 def index():
-  return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
